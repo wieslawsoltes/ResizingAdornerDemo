ed42dfb82de8207c0788edfe7fc520dee11f16c2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-13 14:18:19 +0200

    Update MainWindow.axaml.cs

--- ResizingAdorner/MainWindow.axaml.cs
+++ ResizingAdorner/MainWindow.axaml.cs
@@ -18,6 +18,8 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
+    // DragStarted Implementation
+
     private void DragStarted(object? sender, VectorEventArgs e)
     {
         if (sender is Thumb { Tag: Control control })
@@ -31,6 +33,8 @@ public partial class MainWindow : Window
             // control.Height = _height;
         }
     }
+
+    // DragDelta Implementation
 
     private void DragDeltaCenter(object? sender, VectorEventArgs e)
     {
@@ -50,6 +54,114 @@ public partial class MainWindow : Window
 
             Canvas.SetLeft(control, left);
             Canvas.SetTop(control, top);
+        }
+
+        _updating = false;
+    }
+
+    private void DragDeltaLeft(object? sender, VectorEventArgs e)
+    {
+        if (_updating)
+        {
+            return;
+        }
+
+        _updating = true;
+
+        if (sender is Thumb { Tag: Control control })
+        {
+            Console.WriteLine($"[Left.Delta] {control} {e.Vector}");
+
+            var left = _left + e.Vector.X;
+            var width = _width - e.Vector.X;
+
+            if (width >= 0)
+            {
+                Canvas.SetLeft(control, left);
+                // TODO: Check for MinWidth
+                control.Width = width;
+            }
+        }
+
+        _updating = false;
+    }
+
+    private void DragDeltaRight(object? sender, VectorEventArgs e)
+    {
+        if (_updating)
+        {
+            return;
+        }
+
+        _updating = true;
+
+        if (sender is Thumb { Tag: Control control })
+        {
+            Console.WriteLine($"[Right.Delta] {control} {e.Vector}");
+
+            _width = control.Bounds.Width;
+
+            var width = _width + e.Vector.X;
+
+            if (width >= 0)
+            {
+                // TODO: Check for MinWidth
+                control.Width = width;
+            }
+        }
+
+        _updating = false;
+    }
+
+    private void DragDeltaTop(object? sender, VectorEventArgs e)
+    {
+        if (_updating)
+        {
+            return;
+        }
+
+        _updating = true;
+
+        if (sender is Thumb { Tag: Control control })
+        {
+            Console.WriteLine($"[Top.Delta] {control} {e.Vector}");
+
+            var top = _top + e.Vector.Y;
+            var height = _height - e.Vector.Y;
+
+            if (height >= 0)
+            {
+                Canvas.SetTop(control, top);
+                // TODO: Check for MinHeight
+                control.Height = height;
+            }
+        }
+
+        _updating = false;
+    }
+
+    private void DragDeltaBottom(object? sender, VectorEventArgs e)
+    {
+        if (_updating)
+        {
+            return;
+        }
+
+        _updating = true;
+
+        if (sender is Thumb { Tag: Control control })
+        {
+            Console.WriteLine($"[Bottom.Delta] {control} {e.Vector}");
+
+            _height = control.Bounds.Height;
+
+            var height = _height + e.Vector.Y;
+
+            if (height >= 0)
+            {
+                // TODO: Check for MinHeight
+                control.Height = height;
+            }
         }
 
         _updating = false;
@@ -198,7 +310,29 @@ public partial class MainWindow : Window
         _updating = false;
     }
 
+    // DragStarted Event Handlers
+    
     private void PART_ThumbCenter_OnDragStarted(object? sender, VectorEventArgs e)
+    {
+        DragStarted(sender, e);
+    }
+
+    private void PART_ThumbLeft_OnDragStarted(object? sender, VectorEventArgs e)
+    {
+        DragStarted(sender, e);
+    }
+
+    private void PART_ThumbRight_OnDragStarted(object? sender, VectorEventArgs e)
+    {
+        DragStarted(sender, e);
+    }
+
+    private void PART_ThumbTop_OnDragStarted(object? sender, VectorEventArgs e)
+    {
+        DragStarted(sender, e);
+    }
+
+    private void PART_ThumbBottom_OnDragStarted(object? sender, VectorEventArgs e)
     {
         DragStarted(sender, e);
     }
@@ -223,9 +357,31 @@ public partial class MainWindow : Window
         DragStarted(sender, e);
     }
 
+    // DragDelta Event Handlers
+
     private void PART_ThumbCenter_OnDragDelta(object? sender, VectorEventArgs e)
     {
         DragDeltaCenter(sender, e);
+    }
+
+    private void PART_ThumbLeft_OnDragDelta(object? sender, VectorEventArgs e)
+    {
+        DragDeltaLeft(sender, e);
+    }
+
+    private void PART_ThumbRight_OnDragDelta(object? sender, VectorEventArgs e)
+    {
+        DragDeltaRight(sender, e);
+    }
+
+    private void PART_ThumbTop_OnDragDelta(object? sender, VectorEventArgs e)
+    {
+        DragDeltaTop(sender, e);
+    }
+
+    private void PART_ThumbBottom_OnDragDelta(object? sender, VectorEventArgs e)
+    {
+        DragDeltaBottom(sender, e);
     }
 
     private void PART_ThumbTopLeft_OnDragDelta(object? sender, VectorEventArgs e)
