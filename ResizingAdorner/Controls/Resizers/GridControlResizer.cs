1fcba91d74a5d8194052a145dd4753a6f3f2d46c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-21 00:41:01 +0200

    Create GridControlResizer.cs

---
+++ ResizingAdorner/Controls/Resizers/GridControlResizer.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using ResizingAdorner.Controls.Model;
+
+namespace ResizingAdorner.Controls.Resizers;
+
+public class GridControlResizer : IControlResizer
+{
+    public bool EnableSnap { get; set; }
+
+    public double SnapX { get; set; }
+
+    public double SnapY { get; set; }
+
+    public void Start(Control control)
+    {
+        // TODO:
+    }
+
+    public void Move(Control control, Vector vector)
+    {
+        // TODO:
+    }
+
+    public void Left(Control control, Vector vector)
+    {
+        // TODO:
+    }
+
+    public void Right(Control control, Vector vector)
+    {
+        // TODO:
+    }
+
+    public void Top(Control control, Vector vector)
+    {
+        // TODO:
+    }
+
+    public void Bottom(Control control, Vector vector)
+    {
+        // TODO:
+    }
+}
