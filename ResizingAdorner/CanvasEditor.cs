cdea7e9e73cb914541f0dbc76b4588ce950410f5
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-20 10:24:57 +0200

    Refactor into CanvasEditor

---
+++ ResizingAdorner/CanvasEditor.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+
+namespace ResizingAdorner;
+
+public class CanvasEditor
+{
+    private Canvas? _canvas;
+
+    public Point InsertPoint { get; private set; }
+
+    public void Initialize(Canvas canvas)
+    {
+        _canvas = canvas;
+        _canvas.AddHandler(InputElement.PointerPressedEvent, Canvas_PointerPressed, RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+    }
+
+    public void DeInitialize()
+    {
+        if (_canvas is { })
+        {
+            _canvas.RemoveHandler(InputElement.PointerPressedEvent, Canvas_PointerPressed);
+            _canvas = null;
+        }
+    }
+
+    private void Canvas_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (_canvas is null)
+        {
+            return;
+        }
+
+        if (e.GetCurrentPoint(_canvas).Properties.IsRightButtonPressed)
+        {
+            InsertPoint = e.GetPosition(_canvas);
+        }
+    }
+
+    private void SetControlDefaults(Control control)
+    {
+        switch (control)
+        {
+            case TextBlock textBlock:
+                textBlock.Text = "TextBlock";
+                break;
+            case TextBox textBox:
+                textBox.Text = "TextBox";
+                break;
+            case Label label:
+                label.Content = "Label";
+                break;
+            case CheckBox checkBox:
+                checkBox.Content = "CheckBox";
+                break;
+            case RadioButton radioButton:
+                radioButton.Content = "RadioButton";
+                break;
+            case Slider slider:
+                slider.Value = 50;
+                slider.Width = 150;
+                break;
+            case Ellipse ellipse:
+                ellipse.Fill = new SolidColorBrush(Colors.Gray);
+                ellipse.Width = 100;
+                ellipse.Height = 100;
+                break;
+            case Rectangle rectangle:
+                rectangle.Fill = new SolidColorBrush(Colors.Gray);
+                rectangle.Width = 100;
+                rectangle.Height = 100;
+                break;
+        }
+    }
+
+    public void Insert(Type type, Point point)
+    {
+        if (_canvas is null)
+        {
+            return;
+        }
+
+        var obj = Activator.CreateInstance(type);
+        if (obj is not Control control)
+        {
+            return;
+        }
+
+        SetControlDefaults(control);
+
+        Canvas.SetLeft(control, point.X);
+        Canvas.SetTop(control, point.Y);
+
+        _canvas.Children.Add(control);
+    }
+}
--- ResizingAdorner/MainView.axaml.cs
+++ ResizingAdorner/MainView.axaml.cs
@@ -1,10 +1,5 @@
 using System;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Shapes;
-using Avalonia.Input;
-using Avalonia.Interactivity;
-using Avalonia.Media;
 using Avalonia.VisualTree;
 using ResizingAdorner.Controls.Model;
 using ResizingAdorner.Controls.Selection;
@@ -15,7 +10,7 @@ public partial class MainView : UserControl
 {
     public static readonly IControlSelection? ControlSelection = new ControlSelection();
 
-    private Point _insertPoint;
+    private readonly CanvasEditor _canvasEditor = new ();
 
     public MainView()
     {
@@ -29,16 +24,8 @@ public partial class MainView : UserControl
                 ControlSelection?.Initialize(control);
             }
 
-            Canvas.AddHandler(InputElement.PointerPressedEvent, Canvas_PointerPressed, RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+            _canvasEditor.Initialize(Canvas);
         };
-    }
-
-    private void Canvas_PointerPressed(object? sender, PointerPressedEventArgs e)
-    {
-        if (e.GetCurrentPoint(Canvas).Properties.IsRightButtonPressed)
-        {
-            _insertPoint = e.GetPosition(Canvas);
-        }
     }
 
     public void OnDelete()
@@ -46,54 +33,8 @@ public partial class MainView : UserControl
         ControlSelection?.Delete();
     }
 
-    private void SetControlDefaults(Control control)
-    {
-        switch (control)
-        {
-            case TextBlock textBlock:
-                textBlock.Text = "TextBlock";
-                break;
-            case TextBox textBox:
-                textBox.Text = "TextBox";
-                break;
-            case Label label:
-                label.Content = "Label";
-                break;
-            case CheckBox checkBox:
-                checkBox.Content = "CheckBox";
-                break;
-            case RadioButton radioButton:
-                radioButton.Content = "RadioButton";
-                break;
-            case Slider slider:
-                slider.Value = 50;
-                slider.Width = 150;
-                break;
-            case Ellipse ellipse:
-                ellipse.Fill = new SolidColorBrush(Colors.Gray);
-                ellipse.Width = 100;
-                ellipse.Height = 100;
-                break;
-            case Rectangle rectangle:
-                rectangle.Fill = new SolidColorBrush(Colors.Gray);
-                rectangle.Width = 100;
-                rectangle.Height = 100;
-                break;
-        }
-    }
-
     public void OnInsert(Type type)
     {
-        var obj = Activator.CreateInstance(type);
-        if (obj is Control control)
-        {
-            SetControlDefaults(control);
-
-            Canvas.SetLeft(control, _insertPoint.X);
-            Canvas.SetTop(control, _insertPoint.Y);
-
-            Canvas.Children.Add(control);
-        }
+        _canvasEditor.Insert(type, _canvasEditor.InsertPoint);
     }
 }
-
