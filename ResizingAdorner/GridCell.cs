db2678cab6f596baa4061be0d4aa0cc382b7b07e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-22 21:26:21 +0200

    Update GridCell.cs

--- ResizingAdorner/GridCell.cs
+++ ResizingAdorner/GridCell.cs
@@ -8,5 +8,7 @@ public class GridCell
     public int Row { get; set; }
     public double ActualWidth { get; set; }
     public double ActualHeight { get; set; }
+    public double ColumnOffset { get; set; }
+    public double RowOffset { get; set; }
     public Rect Bounds { get; set; }
 }
