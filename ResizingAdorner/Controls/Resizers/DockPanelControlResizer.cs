bcc4489f4520bdd6c98d92d5cfc66d4b53b7269e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-22 22:13:18 +0200

    Create DockPanelControlResizer.cs

---
+++ ResizingAdorner/Controls/Resizers/DockPanelControlResizer.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using ResizingAdorner.Controls.Model;
+
+namespace ResizingAdorner.Controls.Resizers;
+
+public class DockPanelControlResizer : IControlResizer
+{
+    private DockPanel? _dockPanel;
+
+    public bool EnableSnap { get; set; }
+
+    public double SnapX { get; set; }
+
+    public double SnapY { get; set; }
+
+    public void Start(Control control)
+    {
+        _dockPanel = control.Parent as DockPanel;
+    }
+
+    public void Move(Control control, Point origin, Vector vector)
+    {
+        // TODO:
+    }
+
+    public void Left(Control control, Point origin, Vector vector)
+    {
+        // TODO:
+    }
+
+    public void Right(Control control, Point origin, Vector vector)
+    {
+        // TODO:
+    }
+
+    public void Top(Control control, Point origin, Vector vector)
+    {
+        // TODO:
+    }
+
+    public void Bottom(Control control, Point origin, Vector vector)
+    {
+        // TODO:
+    }
+}
