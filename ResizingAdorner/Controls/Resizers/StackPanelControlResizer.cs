9cac68e1786cf5ce93f3946b5acc0b444767f5fb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-22 22:19:23 +0200

    Update StackPanelControlResizer.cs

--- ResizingAdorner/Controls/Resizers/StackPanelControlResizer.cs
+++ ResizingAdorner/Controls/Resizers/StackPanelControlResizer.cs
@@ -1,12 +1,15 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using ResizingAdorner.Controls.Model;
+using ResizingAdorner.Controls.Utilities;
 
 namespace ResizingAdorner.Controls.Resizers;
 
 public class StackPanelControlResizer : IControlResizer
 {
     private StackPanel? _stackPanel;
+    private double _width;
+    private double _height;
 
     public bool EnableSnap { get; set; }
 
@@ -14,9 +17,21 @@ public class StackPanelControlResizer : IControlResizer
 
     public double SnapY { get; set; }
 
+    private double SnapXValue(double value)
+    {
+        return EnableSnap ? SnapHelper.SnapValue(value, SnapX) : value;
+    }
+
+    private double SnapYValue(double value)
+    {
+        return EnableSnap ? SnapHelper.SnapValue(value, SnapY) : value;
+    }
+
     public void Start(Control control)
     {
         _stackPanel = control.Parent as StackPanel;
+        _width = control.Bounds.Width;
+        _height = control.Bounds.Height;
     }
 
     public void Move(Control control, Point origin, Vector vector)
@@ -27,20 +42,48 @@ public class StackPanelControlResizer : IControlResizer
     public void Left(Control control, Point origin, Vector vector)
     {
         // TODO:
+
+        var width = _width - vector.X;
+        if (width >= 0)
+        {
+            // TODO: Check for MinWidth
+            control.Width = SnapXValue(width);
+        }
     }
 
     public void Right(Control control, Point origin, Vector vector)
     {
         // TODO:
+
+        var width = _width + vector.X;
+        if (width >= 0)
+        {
+            // TODO: Check for MinWidth
+            control.Width = SnapXValue(width);
+        }
     }
 
     public void Top(Control control, Point origin, Vector vector)
     {
         // TODO:
+
+        var height = _height - vector.Y;
+        if (height >= 0)
+        {
+            // TODO: Check for MinHeight
+            control.Height = SnapYValue(height);
+        }
     }
 
     public void Bottom(Control control, Point origin, Vector vector)
     {
         // TODO:
+
+        var height = _height + vector.Y;
+        if (height >= 0)
+        {
+            // TODO: Check for MinHeight
+            control.Height = SnapYValue(height);
+        }
     }
 }
