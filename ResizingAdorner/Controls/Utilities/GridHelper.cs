c2b35cc7b9bf747b4217d17ac6e5cb7f7dd8f663
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-22 21:47:18 +0200

    Update GridHelper.cs

--- ResizingAdorner/Controls/Utilities/GridHelper.cs
+++ ResizingAdorner/Controls/Utilities/GridHelper.cs
@@ -6,6 +6,58 @@ namespace ResizingAdorner.Controls.Utilities;
 
 public static class GridHelper
 {
+    public static List<GridColumn> GetColumns(Grid grid)
+    {
+        var columnsCount = grid.ColumnDefinitions.Count;
+        var columns = new List<GridColumn>();
+
+        var columnOffset = 0d;
+
+        for (var i = 0; i < columnsCount; i++)
+        {
+            var columnDefinition = grid.ColumnDefinitions[i];
+
+            var column = new GridColumn
+            {
+                Column = i,
+                ActualWidth = columnDefinition.ActualWidth,
+                ColumnOffset = columnOffset,
+            };
+
+            columns.Add(column);
+
+            columnOffset += columnDefinition.ActualWidth;
+        }
+
+        return columns;
+    }
+    
+    public static List<GridRow> GetRows(Grid grid)
+    {
+        var rowsCount = grid.RowDefinitions.Count;
+        var rows = new List<GridRow>();
+
+        var rowOffset = 0d;
+
+        for (var i = 0; i < rowsCount; i++)
+        {
+            var rowDefinition = grid.RowDefinitions[i];
+
+            var row = new GridRow
+            {
+                Row = i,
+                ActualHeight = rowDefinition.ActualHeight,
+                RowOffset = rowOffset
+            };
+
+            rows.Add(row);
+
+            rowOffset += rowDefinition.ActualHeight;
+        }
+
+        return rows;
+    }
+
     public static List<GridCell> GetCells(Grid grid)
     {
         var columnsCount = grid.ColumnDefinitions.Count;
