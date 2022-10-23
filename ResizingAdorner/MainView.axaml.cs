22322a3da9a17392a0b022fb94f828e65923ef4b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-23 23:58:57 +0200

    Update MainView.axaml.cs

--- ResizingAdorner/MainView.axaml.cs
+++ ResizingAdorner/MainView.axaml.cs
@@ -1,9 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.VisualTree;
 using ResizingAdorner.Controls.Model;
 using ResizingAdorner.Controls.Selection;
+using ResizingAdorner.Controls.Utilities;
 using ResizingAdorner.Editors;
 
 namespace ResizingAdorner;
@@ -32,6 +38,73 @@ public partial class MainView : UserControl
         };
 
         InitToolbox();
+        
+        ControlTypes.AddHandler(InputElement.PointerPressedEvent, ToolBox_PointerPressed, RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+        ControlTypes.AddHandler(InputElement.PointerReleasedEvent, ToolBox_PointerReleased, RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+        ControlTypes.AddHandler(InputElement.PointerMovedEvent, ToolBox_PointerMoved, RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+    }
+
+    private bool _isPressed;
+    private bool _isDragging;
+    private Point _start;
+    private ListBoxItem? _dragItem;
+    
+    private void ToolBox_PointerMoved(object? sender, PointerEventArgs e)
+    {
+        if (_isPressed)
+        {
+            if (!_isDragging)
+            {
+                var point = e.GetPosition(ControlTypes);
+                var deltaX = Math.Abs((_start - point).X);
+                var deltaY = Math.Abs((_start - point).Y);
+                if (deltaX > 3d || deltaY > 3d)
+                {
+                    _isDragging = true;
+                    Console.WriteLine(_isDragging);
+                }
+            }
+            else
+            {
+                var canvas = this.InputHitTest(e.GetPosition(ControlTypes));
+                Console.WriteLine(canvas);
+                //var canvas = HitTestHelper.HitTest<Canvas>(e, ControlTypes);
+                //if (Equals(canvas, Canvas))
+                //{
+                //    _canvasEditor.Insert(_dragItem.DataContext as Type, e.GetPosition(Canvas));
+                //}
+            }
+        }
+    }
+
+    private void ToolBox_PointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        if (_isDragging)
+        {
+            var canvas = this.InputHitTest(e.GetPosition(ControlTypes));
+            Console.WriteLine(canvas);
+            //var canvas = HitTestHelper.HitTest<Canvas>(e, ControlTypes);
+            if (Equals(canvas, Canvas))
+            {
+                _canvasEditor.Insert(_dragItem.DataContext as Type, e.GetPosition(Canvas));
+            }
+        }
+
+        _isPressed = false;
+        _isDragging = false;
+        _dragItem = null;
+    }
+
+    private void ToolBox_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        var listBoxItem = HitTestHelper.HitTest<ListBoxItem>(e, ControlTypes);
+        if (listBoxItem is { })
+        {
+            _start = e.GetPosition(ControlTypes);
+            _isPressed = true;
+            _isDragging = false;
+            _dragItem = listBoxItem;
+        }
     }
 
     private void InitToolbox()
