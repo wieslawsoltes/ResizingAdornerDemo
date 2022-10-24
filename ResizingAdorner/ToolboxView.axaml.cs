1c2d3c26740bde207709f0434ef6f711519f4ccd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-24 13:08:11 +0200

    Update ToolboxView.axaml.cs

--- ResizingAdorner/ToolboxView.axaml.cs
+++ ResizingAdorner/ToolboxView.axaml.cs
@@ -4,6 +4,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using ResizingAdorner.Controls;
 using ResizingAdorner.Controls.Utilities;
 using ResizingAdorner.Editors;
 
