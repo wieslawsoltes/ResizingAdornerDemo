b64eb1ff54213a16823a36913940c005bbedcc62
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-10-25 23:07:31 +0200

    Update XamlDomNode.cs

--- ResizingAdorner/XamlDom/XamlDomNode.cs
+++ ResizingAdorner/XamlDom/XamlDomNode.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Avalonia;
+using System.Text;
 using Avalonia.Controls;
 using ResizingAdorner.Utilities;
 
@@ -83,4 +83,83 @@ public class XamlDomNode
         return AvaloniaProperty.UnsetValue;
     }
     */
+
+    public void Write(StringBuilder sb, int indentLevel)
+    {
+        if (ControlType is null)
+        {
+            return;
+        }
+
+        var hasContent = false;
+
+        if (indentLevel > 0)
+        {
+            sb.Append(new string(' ', indentLevel * 2));
+        }
+        
+        sb.Append('<');
+        sb.Append(ControlType.Name);
+
+        if (Values is { })
+        {
+            foreach (var kvp in Values)
+            {
+                if (kvp.Value is null)
+                {
+                    continue;
+                }
+                sb.Append(' ');
+                sb.Append(kvp.Key);
+                sb.Append('=');
+                sb.Append('"');
+                sb.Append(kvp.Value);
+                sb.Append('"');
+            }
+        }
+
+        if (Child is { })
+        {
+            sb.Append('>');
+            sb.AppendLine();
+
+            var childLevel = indentLevel + 1;
+
+            Child.Write(sb, childLevel);
+
+            hasContent = true;
+        }
+
+        if (Children is {Count: > 0})
+        {
+            sb.Append('>');
+            sb.AppendLine();
+
+            var childrenLevel = indentLevel + 1;
+
+            foreach (var child in Children)
+            {
+                child.Write(sb, childrenLevel);
+            }
+
+            hasContent = true;
+        }
+
+        if (hasContent)
+        {
+            sb.Append(new string(' ', indentLevel * 2));
+            sb.Append('<');
+            sb.Append('/');
+            sb.Append(ControlType.Name);
+            sb.Append('>');
+            sb.AppendLine();
+        }
+        else
+        {
+            sb.Append(' ');
+            sb.Append('/');
+            sb.Append('>');
+            sb.AppendLine();
+        }
+    }
 }
